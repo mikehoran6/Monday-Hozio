878446d4a1f925e9234bc1a9dc0bd485deffd92a
Mike Horan <mike.horan06@gmail.com>
2021-09-29 13:16:53 -0400

    Inital Commit

---
+++ constants.py
@@ -0,0 +1,3 @@
+apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjEyMjY1NDI2NSwidWlkIjoyMDgyODM5NSwiaWFkIjoiMjAyMS0wOC0zMFQyMDo1ODoxOC42ODdaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6ODQwNTAwMywicmduIjoidXNlMSJ9.ZO2c9M_gmaTIgMCVHIqyFTQq9g5WGSppRtq04nTRHKk"
+apiUrl = "https://api.monday.com/v2"
+headers = {"Authorization" : apiKey}
--- integrations.py
+++ integrations.py
@@ -1,25 +1,31 @@
 import os
 import json
+import requests
+from constants import apiUrl, headers
 from flask import Flask, request, abort, make_response
 app = Flask(__name__)
 
 @app.route('/createtasksgroup', methods=['POST'])
 def create_tasks_group():
-    dispatcher = {'1': function1()}
     logs = open('Logs.txt', 'a')
     if request.method == 'POST':
         hook = request.json
         if 'challenge' in hook:
-            headers = {
-                'Content-Type': 'application/json'
-            }
-            response = make_response(json.dumps(hook), 200, headers)
+            resp_headers = {'Content-Type': 'application/json'}
+            response = make_response(json.dumps(hook), 200, resp_headers)
             return response
+        else:
+            logs.write(hook)
+            client_name = hook['event']['itemName']
+            query = 'mutation { create_group (board_id: 1673060240, group_name: ' + client_name + ') { id }}'
+            data = {'query': query}
+            requests.post(url=apiUrl, json=data, headers=headers)
     else:
         abort(400)
 
-def function1():
-    print()
+def functiondict():
+    dispatcher = {'1': create_tasks_group()}
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
