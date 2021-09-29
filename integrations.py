b541e25741e41177929d030a335ae82358ad7b46
Mike Horan <mike.horan06@gmail.com>
2021-09-29 15:04:19 -0400

    Inital Commit

--- integrations.py
+++ integrations.py
@@ -1,5 +1,6 @@
 import os
 import json
+import sys
 import requests
 from constants import apiUrl, headers
 from flask import Flask, request, abort, make_response
@@ -7,7 +8,6 @@ app = Flask(__name__)
 
 @app.route('/createtasksgroup', methods=['POST'])
 def create_tasks_group():
-    logs = open('Logs.txt', 'a')
     if request.method == 'POST':
         hook = request.json
         if 'challenge' in hook:
@@ -15,16 +15,13 @@ def create_tasks_group():
             response = make_response(json.dumps(hook), 200, resp_headers)
             return response
         else:
-            logs.write(str(hook))
+            sys.stdout.write(str(hook))
             client_name = hook['event']['pulseName']
             query = 'mutation { create_group (board_id: 1673060240, group_name: ' + client_name + ') { id }}'
             data = {'query': query}
             requests.post(url=apiUrl, json=data, headers=headers)
     else:
         abort(400)
-
-def functiondict():
-    dispatcher = {'1': create_tasks_group()}
 
 
 if __name__ == "__main__":
